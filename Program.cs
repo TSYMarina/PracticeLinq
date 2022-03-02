7eee69630501177d1e1286f17b4c312386c8d14e
TSYMarina <marinatsy16@gmail.com>
2022-03-02 14:17:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string> { "Fortnite", "Pacman", "Tetris", "Super Mario", "The Sims", "Grand Theft Auto V", "Sonic The Hedgehog", "CounterStrike", "Minecraft", "Super Smash Brothers", "Solar Ash", "The Legend of Zelda", "Metal Gear Solid", "WoW", "Final Fantasy", "Pokemon"};
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+
+
+
+}
+
